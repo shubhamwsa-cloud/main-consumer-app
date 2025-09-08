e5cc8b5b4828946e45e7e72be57740142032bf47
Shubham Kumar <shubham.kumar.ext@wsa.com>
2025-09-08 17:26:23 +0530

    working ui-catalog as submodule

--- app/src/main/java/com/example/main_consumer_app/MainActivity.kt
+++ app/src/main/java/com/example/main_consumer_app/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.main_consumer_app
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.main_consumer_app.ui.theme.MainconsumerappTheme
+import com.wsa.ui_component.IndicatorContainer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +24,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             MainconsumerappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        IndicatorItem()
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun IndicatorItem() {
+    IndicatorContainer(indicatorColor = Color.Green) {
+        Text(text = "Henosis container", color = Color.Green)
     }
 }
 
